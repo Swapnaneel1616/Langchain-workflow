3a7e267d27bdb41813afb292169225ea6725606b
Swapnaneel <swapnaneel.log.in.16@gmail.com>
2026-03-16 17:54:53 -0400

    Commit

--- chatmodel.py
+++ chatmodel.py
@@ -10,3 +10,10 @@ model = ChatOpenAI(model='gpt-4', temperature=0 , max_completion_tokens=10)
 results = model.invoke("What is the capital of India?")
 
 print(results)
+
+
+#Embeddings 
+embedding = OpenAIEmbeddings(model='text-embedding-3-large', dimensions=32)
+
+embed_result = embedding.embed_query("delhi is the capital of India")
+print(str(embed_result))
