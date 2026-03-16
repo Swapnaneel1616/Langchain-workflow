5953a95c689aae80576ea45f3a475d2bdc7a413b
Swapnaneel <swapnaneel.log.in.16@gmail.com>
2026-03-16 18:01:21 -0400

    Commit

--- chatmodel.py
+++ chatmodel.py
@@ -1,4 +1,6 @@
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np 
 import os 
 from dotenv import load_dotenv
 
@@ -17,3 +19,18 @@ embedding = OpenAIEmbeddings(model='text-embedding-3-large', dimensions=32)
 
 embed_result = embedding.embed_query("delhi is the capital of India")
 print(str(embed_result))
+
+
+#Multiple Embeddings
+documents  = [
+    "Delhi is the capital of India",
+    "kolkata is the capital of West Bengal",
+    "Paris the capital of France"
+]
+
+embed_doc = embedding.embed_documents(documents)
+print(str(embed_doc))
+
+
+
+#Document Similarity
